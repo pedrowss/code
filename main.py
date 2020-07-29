a5f46ae6f1ab192c38cdf5aea9e4f6e210659d4b
pedro siqueira <pedrowss@gmail.com>
2020-07-28 22:39:49 -0300

    Jarbas

---
+++ main.py
@@ -0,0 +1,43 @@
+def int32(x):
+    if(x < -2147483648 or x > 2147483647):
+        return 1
+
+
+def reverse(x):
+    if(int32(x)):
+        return 0
+
+    x = str(x)
+
+    x = x[::-1]
+
+    if(x[len(x)-1] == '-'):
+        x = x[:len(x) - 1]
+        x = "-" + x
+
+    x = int(x)
+
+    if(int32(x)):
+        return 0
+
+    x = str(x)
+
+    if(len(x) == 1 and x[0] == '0'):
+        return 0
+
+    if(x[0] == '0'):
+        x = x[1:len(x)]
+
+    # if(x[len(x)-1] == '-'):
+    #     x = x[:len(x) - 1]
+    #     x = "-" + x
+
+    return int(x)
+
+
+x2 = 1534236469
+# x2 = -123
+# x = str(x2)
+print(reverse(x2))
+
+# 1534236469
